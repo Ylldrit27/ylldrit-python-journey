e2a58d6c1bd0188188febfb517ed4a60c93a8f78
Ylldrit Miftari <ylldrit19@gmail.com>
2025-08-26 18:06:38 +0100

    Variables and data types
    Basic variables and data types

---
+++ variables_and_data_types.py
@@ -0,0 +1,19 @@
+name = 'Ylldrit'  # string
+age = 20  # int
+height = 1.79  # float/double
+is_true = True  # bool
+
+# Introduce yourself
+# print('Hello! My name is ' + name)
+
+# Display your age
+# print('I\'m ' + str(age) + ' years old')   # Note: cannot concatenate integers in strings
+
+# Display height
+# print('I\'m ' + str(height) + ' centimetres tall')
+
+# Display a boolean statement
+if (age == 20):
+    print('Ylldrit is indeed 20')
+else:
+    print('That\'s not quite right')
