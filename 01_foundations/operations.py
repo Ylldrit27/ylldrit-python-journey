a3842a5524f0b3d61678cc1e05ac1d5876592cc8
Ylldrit Miftari <ylldrit19@gmail.com>
2025-08-27 12:59:55 +0100

    Program containing arithmetic operations

---
+++ 01_foundations/operations.py
@@ -0,0 +1,14 @@
+# Addition
+# print(2 + 2)  # 4
+
+# Substraction
+# print(100 - 1)  # 99
+
+# Multiplication
+# print(14 * 14)  # 196
+
+# Exponentiation
+# print(3 ** 4)  # 81
+
+# Modulus (remainder)
+# print(17 % 2)  # 1
