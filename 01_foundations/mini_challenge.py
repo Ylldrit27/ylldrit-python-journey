9a1b3b54e3102fba4e1bef3200bab7920b806612
Ylldrit Miftari <ylldrit19@gmail.com>
2025-08-27 15:07:34 +0100

    Mini challenge
    A calculator that asks for two numbers and prints their sum

---
+++ 01_foundations/mini_challenge.py
@@ -0,0 +1,8 @@
+# A calculator that asks for two numbers and prints their sum
+
+# Ask the user to input two numbers
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+
+# Return the sum of the two numbers provided in input
+print(str(num1) + ' + ' + str(num2) + ' =', num1 + num2)
