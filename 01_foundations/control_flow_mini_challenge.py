7b33b2fe5ac49e8b2a35774d0516c6d64be1fcf8
Ylldrit Miftari <ylldrit19@gmail.com>
2025-09-08 11:53:41 +0100

    Modified program to include more grade boundaries and set up a warning if the user inputs a score outside 0-100

--- 01_foundations/control_flow_mini_challenge.py
+++ 01_foundations/control_flow_mini_challenge.py
@@ -2,13 +2,19 @@
 score = int(input("Enter your score: "))
 
 # Identify the grade
-if score >= 90:
-    print("Grade: A")
-elif score >= 80:
-    print("Grade: B")
-elif score >= 70:
-    print("Grade: C")
-elif score >= 60:
-    print("Grade: D")
+if score >= 90 and score <= 100:
+    print(f"Grade: A*")
+elif score >= 80 and score <= 89:
+    print(f"Grade: A")
+elif score >= 70 and score <= 79:
+    print(f"Grade: B")
+elif score >= 60 and score <= 69:
+    print(f"Grade: C")
+elif score >= 50 and score <= 59:
+    print(f"Grade: D")
+elif score >= 40 and score <= 49:
+    print(f"Grade: E")
+elif score >= 0 and score <= 39:
+    print(f"Grade: F")
 else:
-    print("Grade: F")
+    print(f"Invalid score. Please enter between 0-100.")
